efb5fa07acedb40ee2252f036a052685a95b8cfa
Xin401 <wenxinxu0401@gmail.com>
2024-03-28 02:13:33 +0800

    finished

--- hw1/part2/JBF.py
+++ hw1/part2/JBF.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import cv2
 
@@ -16,5 +15,34 @@ class Joint_bilateral_filter(object):
         padded_guidance = cv2.copyMakeBorder(guidance, self.pad_w, self.pad_w, self.pad_w, self.pad_w, BORDER_TYPE).astype(np.int32)
 
         ### TODO ###
-        
+        padded_guidance = padded_guidance.astype(np.float64) / 255.0
+        guidance = guidance.astype(np.float64) / 255.0
+
+        spatial_kernel = np.zeros((self.wndw_size, self.wndw_size))
+        for x in range(self.wndw_size):
+            for y in range(self.wndw_size):
+                spatial_kernel[x, y] = np.exp(-((x - self.wndw_size//2)**2 + (y - self.wndw_size//2)**2) / (2 * self.sigma_s**2))
+
+
+        output = np.zeros_like(img)
+
+
+        for i in range(img.shape[0]):
+            for j in range(img.shape[1]):
+                window_img = padded_img[i:i+self.wndw_size, j:j+self.wndw_size, :]
+                window_guidance = padded_guidance[i:i+self.wndw_size, j:j+self.wndw_size]
+
+                if guidance.ndim == 3:
+                    range_kernel = np.exp(-((window_guidance - guidance[i,j])**2).sum(axis=2) /  (2*self.sigma_r**2))
+                else:
+                    range_kernel = np.exp(-((window_guidance - guidance[i,j])**2) / (2*self.sigma_r**2))
+
+                jbf = spatial_kernel * range_kernel
+
+                output[i, j, :] = np.sum(window_img * jbf[:, :, np.newaxis], axis=(0, 1)) / np.sum(jbf)
+
+
         return np.clip(output, 0, 255).astype(np.uint8)
+    
+
+    
