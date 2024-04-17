8bc0d9597135a3a75834a1df25ed087071a0071a
Xin <wenxinxu0401@gmail.com>
2024-04-18 03:21:57 +0800

    hw2 finished

--- hw2/p2/config.py
+++ hw2/p2/config.py
@@ -8,12 +8,12 @@
 exp_name   = 'adam_da' # name of experiment
 
 # Model Options
-model_type = 'resnet18' # 'mynet' or 'resnet18'
-# model_type = 'mynet' # 'mynet' or 'resnet18'
+# model_type = 'resnet18' # 'mynet' or 'resnet18'
+model_type = 'mynet' # 'mynet' or 'resnet18'
 
 # Learning Options
 epochs     = 20           # train how many epochs
 batch_size = 32           # batch size for dataloader 
-use_adam   = True        # Adam or SGD optimizer
-lr         = 1e-4         # learning rate
+use_adam   = False       # Adam or SGD optimizer
+lr         = 1e-2         # learning rate
 milestones = [10, 32, 45] # reduce learning rate at 'milestones' epochs
--- hw2/p2/model.py
+++ hw2/p2/model.py
@@ -12,16 +12,19 @@ class MyNet(nn.Module):
         # input channel is 3, and the output dimension is 10 (class).  #
         ################################################################
         self.cnn = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Flatten(),
-            nn.Linear(128 * 8 * 8, 64),
+            nn.Linear(64 * 4 * 4, 512),
             nn.ReLU(),
-            nn.Linear(64, 10)
+            nn.Linear(512, 10)
         )
 
     def forward(self, x):
